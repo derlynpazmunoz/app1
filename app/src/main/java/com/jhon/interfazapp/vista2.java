49a32a862dbdbb656b4d6a5bbfca93bde8cc3903
jhon capote <capotejhon173@gmail.com>
2023-04-19 21:04:05 -0500

    Initial commit

---
+++ app/src/main/java/com/jhon/interfazapp/vista2.java
@@ -0,0 +1,14 @@
+package com.jhon.interfazapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class vista2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vista2);
+    }
+}
